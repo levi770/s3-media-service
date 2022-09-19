243f8bbe66b26a9dee48abc122df7cfa8804880c
levi770 <leviyd@protonmail.com>
2022-09-19 21:52:34 +0200

    add file processor to sqs queue message handler

--- src/sqs-consumer/sqs-consumer.service.ts
+++ src/sqs-consumer/sqs-consumer.service.ts
@@ -4,35 +4,40 @@ import {
   SqsMessageHandler,
   SqsProcess,
 } from '@nestjs-packages/sqs';
+import { InjectQueue } from '@nestjs/bull';
 import { Injectable } from '@nestjs/common';
 import { SQS } from 'aws-sdk';
+import { Queue } from 'bull';
+import { NewObjectParamsDto } from '../common/dto/newObjectParams.dto';
+import { DbManagerService } from '../db-manager/db-manager.service';
 
 @Injectable()
 @SqsProcess(/** name: */ 'levi770devQueue')
 export class SqsConsumerService {
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  constructor() {}
-
-  // @SqsMessageHandler('levi770devQueue', false)
-  // async handleMessage(message: SQS.Message) {
-  //   const obj: any = JSON.parse(message.Body) as {
-  //     message: string;
-  //     date: string;
-  //   };
-  //   const { data } = JSON.parse(obj.Message);
-
-  //   // use the data and consume it the way you want //
-  // }
+  constructor(
+    private readonly dbManagerService: DbManagerService,
+    @InjectQueue('file') private readonly fileQueue: Queue,
+  ) {}
 
   @SqsMessageHandler(/** batch: */ false)
   public async handleMessage(message: SQS.Message) {
-    console.log(message);
-    //TODO image processing
+    const msgBody: any = JSON.parse(message.Body);
+    const objKey = msgBody.Records[0].s3.object.key;
+    const obj = await this.dbManagerService.findByPk(objKey);
+    const objParams = obj.params as NewObjectParamsDto;
+
+    obj.status = 'uploaded';
+    obj.save();
+
+    if (obj.type === 'original' && objParams.optimize) {
+      await this.fileQueue.add('optimize-image', obj);
+    } else if (obj.type != 'original') {
+      obj.status = 'uploaded';
+      obj.save();
+    }
   }
 
   @SqsConsumerEventHandler(/** eventName: */ SqsConsumerEvent.PROCESSING_ERROR)
-  public onProcessingError(error: Error, message: SQS.Message) {
-    console.log(error);
-    console.log(message);
-  }
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-empty-function
+  public onProcessingError(error: Error, message: SQS.Message) {}
 }
