82cd2e0358e2eb11b288fa82c51f65304408549b
levi770 <leviyd@protonmail.com>
2022-09-19 21:46:54 +0200

    fix worker file path

--- src/sqs-consumer/sqs-consumer.module.ts
+++ src/sqs-consumer/sqs-consumer.module.ts
@@ -1,25 +1,18 @@
 import { SqsModule, SqsQueueType } from '@nestjs-packages/sqs';
 import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
-import { SequelizeModule } from '@nestjs/sequelize';
 import { join } from 'path';
 import { SqsConsumerService } from './sqs-consumer.service';
-import { File } from '../models/file.model';
+import { DbManagerModule } from '../db-manager/db-manager.module';
 
 @Module({
   imports: [
-    SequelizeModule.forFeature([File]),
     BullModule.registerQueue({
       name: 'file',
       processors: [
         {
           name: 'optimize-image',
-          path: join(
-            __dirname,
-            `../processor/optimize-image.processor.${
-              process.env.NODE_ENV === 'dev' ? 'ts' : 'js'
-            }`,
-          ),
+          path: join(__dirname, `../processors/optimize-image.processor.ts`),
         },
       ],
     }),
@@ -35,6 +28,7 @@ import { File } from '../models/file.model';
       },
       producerOptions: {},
     }),
+    DbManagerModule,
   ],
   providers: [SqsConsumerService],
 })
