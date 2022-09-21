f4a02200c082d330a5a82aab711fe8d8aafa1f83
levi770 <leviyd@protonmail.com>
2022-09-21 21:38:36 +0200

    debug condition check in sqs message handler

--- src/sqs-consumer/sqs-consumer.service.ts
+++ src/sqs-consumer/sqs-consumer.service.ts
@@ -29,7 +29,7 @@ export class SqsConsumerService {
     obj.status = 'uploaded';
     obj.save();
 
-    if (obj.type === 'original' && objParams.optimize) {
+    if (obj.type === 'original' && objParams.optimize === 'true') {
       await this.fileQueue.add('optimize-image', obj);
     }
   }
