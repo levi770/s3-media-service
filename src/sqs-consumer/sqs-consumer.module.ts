ef374c73ee09da3c26fdc0d03dc9f7cd086f1317
levi770 <leviyd@protonmail.com>
2023-08-11 11:28:07 +0200

    update project name

--- src/sqs-consumer/sqs-consumer.module.ts
+++ src/sqs-consumer/sqs-consumer.module.ts
@@ -14,7 +14,7 @@ import { DbManagerModule } from '../db-manager/db-manager.module';
           name: 'optimize-image',
           path: join(
             __dirname,
-            `../sqs-processors/optimize-image.processor.js`,
+            `../sqs-processors/optimize-image.processor.ts`,
           ),
         },
       ],
